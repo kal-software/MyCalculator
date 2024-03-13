7ca77866ee62f8ac6aa0b88f41b8eb7c552ede54
kalkidan-solomon-coder <152845891+kalkidan-solomon-coder@users.noreply.github.com>
2024-03-14 00:38:51 +0300

    change 2

--- calculator/src/Calculate.jsx
+++ calculator/src/Calculate.jsx
@@ -20,30 +20,30 @@ function Calculate(){
 return (
     <>
     <div className="calculator-div">
-        <h2>Calculator</h2>
-        <h2>Standard</h2>
-        <p>{calculat}</p>
+        <h2 className="calculate-h2">Calculator</h2>
+        <h2 className="calculator-h2">Standard</h2>
+        <p className="calculate-p">{calculat}</p>
         <div className="calculate-button">
             <button onClick={()=>calculation("(")} className="open-brace">(</button>
             <button onClick={()=>calculation(")")} className="open-brace">)</button>
             <button onClick={clear} className="x">x</button>
             <button onClick={()=>calculation("%")} className="module">%</button><br/>
             <button onClick={()=>calculation(7)} className="seven">7</button>
-            <button onClick={()=>calculation(8)}>8</button>
-            <button onClick={()=>calculation(9)}>9</button>
-            <button onClick={()=>calculation('*')}>*</button><br/>
-            <button onClick={()=>calculation(4)}>4</button>
-            <button onClick={()=>calculation(5)}>5</button>
-            <button onClick={()=>calculation(6)}>6</button>
-            <button onClick={()=>calculation('-')}>-</button><br/>
-            <button onClick={()=>calculation(1)}>1</button>
-            <button onClick={()=>calculation(2)}>2</button>
-            <button onClick={()=>calculation(3)}>3</button>
-            <button onClick={()=>calculation('+')}>+</button><br/>
-            <button onClick={()=>calculation('/')}>/</button>
-            <button onClick={()=>calculation(0)}>0</button>
-            <button onClick={()=>calculation('.')}>.</button>
-            <button onClick={()=>result()}>=</button><br/>
+            <button onClick={()=>calculation(8)} className="eight">8</button>
+            <button onClick={()=>calculation(9)} className="nine">9</button>
+            <button onClick={()=>calculation('*')} className="star">*</button><br/>
+            <button onClick={()=>calculation(4)} className="four">4</button>
+            <button onClick={()=>calculation(5)} className="five">5</button>
+            <button onClick={()=>calculation(6)} className="six">6</button>
+            <button onClick={()=>calculation('-')} className="minus">-</button><br/>
+            <button onClick={()=>calculation(1)} className="one">1</button>
+            <button onClick={()=>calculation(2)} className="two">2</button>
+            <button onClick={()=>calculation(3)} className="three">3</button>
+            <button onClick={()=>calculation('+')} className="pluse">+</button><br/>
+            <button onClick={()=>calculation('/')} className="over">/</button>
+            <button onClick={()=>calculation(0)} className="zero">0</button>
+            <button onClick={()=>calculation('.')} className="point">.</button>
+            <button onClick={()=>result()} className="equal">=</button><br/>
         </div>
     </div>
     </>
